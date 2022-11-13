1c11b02ebf8ce2cb0970d33d72beb8415967e169
romeusorionaet <romeusoares14569@gmail.com>
2022-11-13 15:41:39 -0300

    settingsVolume

--- js/index.js
+++ js/index.js
@@ -228,7 +228,7 @@ buttonFirePlace.addEventListener('click', function () {
 // eventos que controla volume pelo input 
 volumeForest.addEventListener('input', function () {
   audioForest.volume = volumeForest.value,
-  audioForest.play()
+  audioForest.pause()
 })
 
 volumeRain.addEventListener('input', function () {
